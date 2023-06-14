0cccfa82d6b5cfa8b832120f475ff0a731a46f6a
n0str <team@codex.so>
2023-06-15 00:52:10 +0300

    Remove server:host

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 export default defineConfig({
-  plugins: [ vue() ],
-  server: {
-    host: true
-  }
+  plugins: [ vue() ]
 });
