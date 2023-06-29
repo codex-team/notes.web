f73a21d23e442566faf741cb402c0b3d89134391
Alexander Chernyaev <265195@niuitmo.ru>
2023-06-29 11:28:37 +0300

    feat: add component EditorJS

--- src/presentation/pages/Home.vue
+++ src/presentation/pages/Home.vue
@@ -2,12 +2,16 @@
   <div>
     <h1>Welcome to the Home Page</h1>
     <p>This is the home page content.</p>
+    <editor></editor>
   </div>
 </template>
 
 <script lang="ts">
+import Editor from '../components/editorjs/Editor.vue';
+
 export default {
   name: 'Home',
+  components: { Editor },
 };
 </script>
 
