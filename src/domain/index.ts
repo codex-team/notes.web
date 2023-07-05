f9ee822dacc446f491ed482d7b31ea952e84dab7
slaveeks <chernyshev2001@icloud.com>
2023-07-06 01:06:08 +0300

    Added getting api url from env

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -2,9 +2,14 @@ import NoteService from './note.service';
 import { init as initRepositories } from '../infrastructure';
 
 /**
+ * Get API url from environment
+ */
+const apiUrl = import.meta.env.VITE_API_URL;
+
+/**
  * Init repositories
  */
-const repositories = initRepositories('http://localhost:3000');
+const repositories = initRepositories(apiUrl);
 
 /**
  * Init services
