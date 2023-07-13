94f253b6fa73cf340563a58a5547fcbd2652bc58
slaveeks <chernyshev2001@icloud.com>
2023-07-13 15:30:03 +0300

    Added base for header

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import i18n from './application/i18n';
 import router from './application/router/index';
-
+import './presentation/styles/colors.pcss';
+import './presentation/styles/sizes.pcss';
 const app = createApp(App);
 
 app.use(router);
---
+++ src/presentation/layout/HeaderLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "HeaderLayout"
+}
+</script>
+
+<style scoped>
+  .header {
+    height: 52px;
+    padding: var(--spacing-ms) var(--spacing-l);
+    background-color: var(--bg-color-header);
+    display: flex;
+    align-items: center;
+    width: 100%;
+  }
+</style>
