db2f8c17140f90fa49ed5ee0f5a784f893bb9b34
slaveeks <chernyshev2001@icloud.com>
2023-07-28 21:37:58 +0300

    Removed import normalize.css from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import 'normalize.css';
 import i18n from '@/application/i18n';
 import router from '@/application/router/index';
 import '@/presentation/styles/index.pcss';
