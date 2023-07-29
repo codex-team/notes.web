235024e7c9c8ccafc7e76470092037b6dbd6e925
Peter Savchenko <specc.dev@gmail.com>
2023-07-29 14:25:28 +0300

    Update Note.vue

--- src/presentation/pages/Note.vue
+++ src/presentation/pages/Note.vue
@@ -11,7 +11,7 @@
 </template>
 
 <script lang="ts" setup>
-import Editor from '@/presentation/components/Editor/Editor.vue';
+import Editor from '@/presentation/components/editor/Editor.vue';
 import useNote from '@/application/services/useNote';
 
 const props = defineProps({
