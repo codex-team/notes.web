2e68438c8d30144ca241751fd98e6a524e40f7c8
Peter Savchenko <specc.dev@gmail.com>
2023-07-30 05:22:46 +0300

    Update index.ts

--- src/domain/event-bus/index.ts
+++ src/domain/event-bus/index.ts
@@ -1,5 +1,5 @@
 /**
- * Event Bus provides a loosely coupled communication way between modules
+ * Event Bus provides a loosely coupled communication way between Domain and some other layers
  *
  * Extends native event emitter called EventTarget
  */
