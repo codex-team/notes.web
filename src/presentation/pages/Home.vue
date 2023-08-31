cf0dc4c8aa56c8f199f92ab43d8bc16bf77d8254
ksenia_subbotina <ks@didlogic.com>
2023-08-31 15:21:52 +0300

    fix(typography): transfer styles to typography

--- src/presentation/pages/Home.vue
+++ src/presentation/pages/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="page__block">
-    <h1 class="page__title">
+  <div class="block">
+    <h1>
       Recent Notes
     </h1>
-    <h2 class="page__title--2">
+    <h2>
       What does it mean «block-styled editor»
     </h2>
-    <h3 class="page__title--3">
+    <h3>
       What does it mean «block-styled editor»
     </h3>
-    <p class="page__content">
+    <p>
       Workspace in classic editors is made of a single contenteditable element, used to create different HTML markups. Editor.js workspace consists of separate Blocks: paragraphs, headings, images, lists, quotes, etc. Each of them is an independent contenteditable element (or more complex structure) provided by Plugin and united by Editor's Core.
     </p>
-    <p class="page__content-small">
+
+    <p class="small">
       Workspace in classic editors is made of a single contenteditable element, used to create different HTML markups. Editor.js workspace consists of separate Blocks: paragraphs, headings, images, lists, quotes, etc. Each of them is an independent contenteditable element (or more complex structure) provided by Plugin and united by Editor's Core.
     </p>
   </div>
@@ -22,26 +23,5 @@
 </script>
 
 <style lang="postcss" scoped>
-@import '@/presentation/styles/typography.pcss';
-
-h1 {
-  @apply --text-heading-1;
-}
-
-h2 {
-  @apply --text-heading-2;
-}
-
-h3 {
-  @apply --text-heading-3;
-}
-
-p {
-  @apply --text-body;
-}
-
-.page__content-small {
-  @apply --text-small;
-}
 
 </style>
