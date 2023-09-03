e3c5f9d6739195fe3de7796dab2a4862bbc5176d
ksenia_subbotina <ks@didlogic.com>
2023-09-03 10:05:08 +0300

    fix(typography): add typography for note

--- src/presentation/pages/Home.vue
+++ src/presentation/pages/Home.vue
@@ -1,21 +1,7 @@
 <template>
-  <div class="block">
-    <h1>
-      Recent Notes
-    </h1>
-    <h2>
-      What does it mean «block-styled editor»
-    </h2>
-    <h3>
-      What does it mean «block-styled editor»
-    </h3>
-    <p>
-      Workspace in classic editors is made of a single contenteditable element, used to create different HTML markups. Editor.js workspace consists of separate Blocks: paragraphs, headings, images, lists, quotes, etc. Each of them is an independent contenteditable element (or more complex structure) provided by Plugin and united by Editor's Core.
-    </p>
-
-    <p class="small">
-      Workspace in classic editors is made of a single contenteditable element, used to create different HTML markups. Editor.js workspace consists of separate Blocks: paragraphs, headings, images, lists, quotes, etc. Each of them is an independent contenteditable element (or more complex structure) provided by Plugin and united by Editor's Core.
-    </p>
+  <div>
+    <h1>Recent Notes</h1>
+    <p>This page will contain your Notes you recently worked with</p>
   </div>
 </template>
 
