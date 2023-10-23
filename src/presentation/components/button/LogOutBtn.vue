aab02c04159ab26f8f4c9a2fa48d19be42edfb62
Shakhzod Sharifov <thedigitaluzb@gmail.com>
2023-10-23 23:27:23 +0500

    Added Button to Logout in Settings

---
+++ src/presentation/components/button/LogOutBtn.vue
@@ -0,0 +1,36 @@
+<template>
+    <Button 
+  text="Log out"
+  :icon="IconUnlink"
+  type="primary"
+  @click="logOut"
+  />
+</template>
+
+<script setup lang="ts">
+import { IconUnlink } from '@codexteam/icons';
+import Button from './Button.vue';
+import { authService } from '@/domain/index';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+/**
+ * Function to logout the user by deleting the refresh 
+ * token from the local storage
+ */
+
+ async function logOut() {
+    
+    await authService.logout().then(() => {
+    router.push({ path: "/" }).then(() => {
+      window.location.reload();
+    });
+  });
+
+      
+      
+  }
+ 
+
+</script>
--- src/presentation/pages/Settings.vue
+++ src/presentation/pages/Settings.vue
@@ -1,11 +1,13 @@
 <template>
   <h1>{{ t('settings.title') }}</h1>
   <ThemeButton />
+  <LogOutBtn/>
 </template>
 
 <script lang="ts" setup>
 import { useI18n } from 'vue-i18n';
 import ThemeButton from '@/presentation/components/theme/ThemeButton.vue';
+import LogOutBtn from '../components/button/LogOutBtn.vue';
 
 const { t } = useI18n();
 </script>
