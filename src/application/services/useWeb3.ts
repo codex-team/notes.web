68ad3debf2853dabdc7e8eec28b521d025cc9fa4
Nikita Melnikov <nikmel2803@gmail.com>
2023-11-25 22:08:10 +0100

    add more providers && test transactions

--- src/application/services/useWeb3.ts
+++ src/application/services/useWeb3.ts
@@ -1,8 +1,57 @@
 import { createSharedComposable } from '@vueuse/core';
-import Onboard from '@web3-onboard/core'
-// import { init as initWeb3Onboard, useOnboard } from '@web3-onboard/vue';
+import { init as initWeb3Onboard, useOnboard } from '@web3-onboard/vue';
 import injectedModule from '@web3-onboard/injected-wallets';
+import coinbaseWalletModule from '@web3-onboard/coinbase';
+import walletConnectModule from '@web3-onboard/walletconnect';
+import {mainnet} from "viem/chains";
+import {createPublicClient, createWalletClient, custom, http} from "viem";
 
+const abi = [
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_to",
+        "type": "address"
+      },
+      {
+        "name": "_value",
+        "type": "uint256"
+      }
+    ],
+    "name": "transfer",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+]
+
+const wcInitOptions = {
+  /**
+   * Project ID associated with [WalletConnect account](https://cloud.walletconnect.com)
+   */
+  projectId: '',
+  /**
+   * Chains required to be supported by all wallets connecting to your DApp
+   */
+  requiredChains: [1],
+  /**
+   * Chains required to be supported by all wallets connecting to your DApp
+   */
+  optionalChains: [42161, 8453, 10, 137, 56],
+  /**
+   * Defaults to `appMetadata.explore` that is supplied to the web3-onboard init
+   * Strongly recommended to provide atleast one URL as it is required by some wallets (i.e. MetaMask)
+   * To connect with WalletConnect
+   */
+  dappUrl: 'http://YourAwesomeDapp.com'
+}
 
 export const useWeb3 = createSharedComposable(() => {
   // const projectId = 'b9edxxxxxxxxxxxxxxxxxx9';
@@ -41,13 +90,20 @@ export const useWeb3 = createSharedComposable(() => {
 
   console.log('injected', injected);
 
+  const coinbaseWalletSdk = coinbaseWalletModule({ darkMode: true });
+
+  const walletConnect = walletConnectModule(wcInitOptions)
 
   // Only one RPC endpoint required per chain
   const rpcAPIKey = import.meta.env.VITE_RPC_API_KEY;
   const rpcUrl = `https://mainnet.infura.io/v3/${rpcAPIKey}`;
 
-  const web3Onboard = Onboard({
-    wallets: [ injected ],
+  const web3Onboard = initWeb3Onboard({
+    wallets: [
+      injected,
+      coinbaseWalletSdk,
+      walletConnect,
+    ],
     chains: [
       {
         id: '0x1',
@@ -58,18 +114,20 @@ export const useWeb3 = createSharedComposable(() => {
     ],
   });
 
-  console.log('web3Onboard', web3Onboard);
 
-
-  // if (connectedWallet) {
-  // // if using ethers v6 this is:
-  // // ethersProvider = new ethers.BrowserProvider(wallet.provider, 'any')
+  // if (web3Onboard.connectWallet) {
+  // if using ethers v6 this is:
+  // ethersProvider = new ethers.BrowserProvider(wallet.provider, 'any')
   //   const ethersProvider = new ethers.providers.Web3Provider(
   //     connectedWallet.provider,
   //     'any'
   //   );
-  // // ..... do stuff with the provider
-  // }
+  // ..... do stuff with the provider
+
+  //   const client = createPublicClient({
+  //     chain: mainnet,
+  //     transport: http(web3Onboard.state),
+  //   })
   // }
 
   /**
@@ -79,7 +137,44 @@ export const useWeb3 = createSharedComposable(() => {
     console.log('openModal');
     // const { connectWallet } = useOnboard();
 
-    const wallets = await web3Onboard.connectWallet();
+    const { connectWallet, disconnectConnectedWallet, connectedWallet } = useOnboard()
+    const wallets = await connectWallet();
+
+    if (connectedWallet) {
+      // if using ethers v6 this is:
+      // ethersProvider = new ethers.BrowserProvider(wallet.provider, 'any')
+      // const ethersProvider = new ethers.providers.Web3Provider(connectedWallet.provider, 'any')
+      // ..... do stuff with the provider
+
+      console.log('connectedWallet')
+      console.log(connectedWallet)
+      const client = createPublicClient({
+        chain: mainnet,
+        transport: custom(connectedWallet.value?.provider!),
+      })
+
+      const walletClient = createWalletClient({
+        account: connectedWallet.value?.accounts[0].address as `0x${string}`,
+        chain: mainnet,
+        transport: custom(connectedWallet.value?.provider!)
+      })
+
+      const blockNumber = await client.getBlockNumber()
+      console.log(blockNumber)
+
+      await walletClient.writeContract({
+        address: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+        abi: abi,
+        functionName: 'transfer',
+        args: ['0xFbDa07a729d5649Da74C58F8F01613FA842323fe', 10000000n],
+      })
+
+      // const hash = await walletClient.sendTransaction({
+      //   to: '0xFbDa07a729d5649Da74C58F8F01613FA842323fe',
+      //   value: 50_000_000_000_000_000n
+      // })
+      // console.log({hash})
+    }
 
     console.log('wallets', wallets);
   }
