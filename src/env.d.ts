a387ad656ce1e7ab6e742b3f4644100abf78e6b3
iamgabrielsoft <iamgabrielsoft@gmail.com>
2024-01-25 21:02:11 +0100

    added description to .envd.ts file

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,11 +1,24 @@
 
 /**
- * @description Typescript Intellisense for VITE Environmental variables
+ * @description - Typescript Intellisense for VITE Environmental variables
  * Add VITE local variables here
  */
 interface ImportMetaEnv {
+  /**
+   * Notex hosted backend server url from environmental variable file(.env)
+   */
   VITE_API_URL: string;
+  /**
+   * Notex google oauth url from environmental variable file (.env )
+   */
   VITE_GOOGLE_OAUTH_URL: string;
+  /**
+   * local backend server url from environmental variable file (.env)
+   */
   VITE_API_LOCAL_URL: string;
+  /**
+   * local google oauth url from environmental variable file (.env)
+   * Add your preferred google oauth url
+   */
   VITE_GOOGLE_LOCAL_OAUTH_URL: string;
 }
