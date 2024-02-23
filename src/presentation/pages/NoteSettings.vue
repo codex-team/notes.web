d01cfdaf8b0a34039a71714eefd1ece4245472cf
elizachi <lizachig2003@gmail.com>
2024-02-23 17:48:04 +0300

    Build fix

--- src/presentation/pages/NoteSettings.vue
+++ src/presentation/pages/NoteSettings.vue
@@ -8,7 +8,7 @@
       :placeholder="'example: landing.codex.so'"
     />
     <Checkbox
-      :id="noteSettings.id"
+      :id="noteSettings.id as unknown as string"
       v-model:checked="noteSettings.isPublic"
       label="is note public"
     />
