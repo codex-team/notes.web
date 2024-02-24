f617aec468e35b80ba4604206ca3f12e83c6ffcc
Tatiana Fomina <fomina.tatianaaa@yandex.ru>
2024-02-24 13:10:55 +0300

    Add hawk integration (#83)
    * Add hawk integration
    
    * Fix eslint errors
    
    * Rewrite to plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      /* eslint-disable-next-line @typescript-eslint/naming-convention */
-      '@/': '/src/',
+import hawkVitePlugin from '@hawk.so/vite-plugin';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      vue(),
+      hawkVitePlugin({
+        token: env.VITE_HAWK_TOKEN,
+      }),
+    ],
+    resolve: {
+      alias: {
+        /* eslint-disable-next-line @typescript-eslint/naming-convention */
+        '@/': '/src/',
+      },
     },
-  },
+  };
 });
