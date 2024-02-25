3b1fd497aa279dcf75d3245ae7a6c2af39330fd5
Peter Savchenko <specc.dev@gmail.com>
2024-02-25 23:53:29 +0300

    feat(ui): button styles

--- codex-ui/src/vue/button/Button.vue
+++ codex-ui/src/vue/button/Button.vue
@@ -1,25 +1,63 @@
 <template>
-  <div>
-    <button :class="[$style.button, `${$style.button}--${size}`, 'text-ui-base-medium']">
-      <slot />
-    </button>
-  </div>
+  <button
+    :class="[$style.button, `${$style.button}--${size}`, `${$style.button}--${style}`, 'text-ui-base-medium']"
+    :theme-accent="style === 'destructive' ? 'red' : undefined"
+  >
+    <slot />
+  </button>
 </template>
 
 <script setup lang="ts">
-import { defineProps } from 'vue';
+import { computed, defineProps } from 'vue';
 
-withDefaults(
+const props = withDefaults(
   defineProps<{
     /**
      * The size of the button
      */
     size?: 'small' | 'medium' | 'large';
+
+    /**
+     * Whether the button is a secondary button
+     */
+    secondary?: boolean;
+
+    /**
+     * Whether the button is disabled
+     */
+    disabled?: boolean;
+
+    /**
+     * Whether the button is responsible for a destructive action
+     */
+    destructive?: boolean;
   }>(),
   {
     size: 'medium',
+    secondary: false,
+    disabled: false,
+    destructive: false,
   }
 );
+
+/**
+ * Button style: primary (default), secondary, destructive, disabled
+ */
+const style = computed(() => {
+  if (props.disabled) {
+    return 'disabled';
+  }
+
+  if (props.secondary) {
+    return 'secondary';
+  }
+
+  if (props.destructive) {
+    return 'destructive';
+  }
+
+  return 'primary';
+});
 </script>
 
 <style lang="postcss" module>
@@ -27,12 +65,17 @@ withDefaults(
   border: 0;
   outline: 0;
   font-family: inherit;
+  cursor: pointer;
 
   --padding: 0 0;
   --radius: 0;
   --color: var(--base--text-solid-foreground);
   --bg: var(--base--solid);
+  --bg-hover: var(--base--solid-hover);
 
+  /**
+   * Sizes
+   */
   &--small {
     --padding: var(--spacing-xxs) var(--spacing-s);
     --radius: var(--radius-m);
@@ -48,9 +91,38 @@ withDefaults(
     --radius: var(--radius-ml);
   }
 
+  /**
+   * Styles
+   */
+  &--primary {
+    --bg: var(--base--solid);
+    --bg-hover: var(--base--solid-hover);
+  }
+
+  &--secondary {
+    --bg: var(--base--bg-secondary);
+    --bg-hover: var(--base--bg-secondary-hover);
+  }
+
+  &--disabled {
+    --bg: var(--base--bg-secondary);
+    --bg-hover: var(--base--bg-secondary);
+    --color: var(--base--text-secondary);
+    cursor: not-allowed;
+  }
+
+  &--destructive {
+    --bg: var(--accent--solid);
+    --bg-hover: var(--accent--solid-hover);
+  }
+
   padding: var(--padding);
   border-radius: var(--radius);
   background-color: var(--bg);
   color: var(--color);
+
+  &:hover {
+    background-color: var(--bg-hover);
+  }
 }
 </style>
