3d9b4c64ec601cd60270c065dc5fe0b6336e477c
iamgabrielsoft <iamgabrielsoft@gmail.com>
2024-02-27 17:09:19 +0100

    addition of hawk var

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,17 +1,12 @@
-
 /**
  * @description - Typescript Intellisense for VITE Environmental variables
  * Add VITE local variables here
  */
 interface ImportMetaEnv {
-  /**
-   * NoteX API server URL
-   */
+  /** NoteX API server URL */
   VITE_API_URL: string;
 
-  /**
-   * Google OAuth redirect URL
-   */
+  /** Google OAuth redirect URL */
   VITE_GOOGLE_OAUTH_URL: string;
 
   /** NoteX default production hostname */
