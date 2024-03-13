ecea577f2a314fa5254ee7cefb44b4c1605da4fd
elizachi <lizachig2003@gmail.com>
2024-03-13 16:55:45 +0300

    Now we have select list for changing user role

--- src/presentation/components/team/Member.vue
+++ src/presentation/components/team/Member.vue
@@ -3,22 +3,32 @@
     <div class="member-name">
       {{ teamMember.user.name || teamMember.user.email }}
     </div>
-    <div v-if="teamMember.id != 1">
+    <select>
+      <option
+        v-for="(role, index) in stringRoles"
+        :key="index"
+        :selected="teamMember.role == index"
+      >
+        {{ role }}
+      </option>
+    </select>
+    <!-- <div v-if="teamMember.id != 1">
       <Checkbox
         :checked="teamMember.role === 1"
         :label="t('noteSettings.team.canEdit')"
         @update:checked="changeMemberRole"
       />
-    </div>
+    </div> -->
   </li>
 </template>
 
 <script setup lang="ts">
 import Checkbox from '@/presentation/components/checkbox/Checkbox.vue';
-import { TeamMember } from '@/domain/entities/Team.ts';
+import { MemberRole, TeamMember } from '@/domain/entities/Team.ts';
 import { useI18n } from 'vue-i18n';
 import useNoteSettings from '@/application/services/useNoteSettings';
 import { NoteId } from '@/domain/entities/Note';
+import { computed } from 'vue';
 
 /**
  * TeamMember props
@@ -44,6 +54,8 @@ async function changeMemberRole() {
 }
 
 const { t } = useI18n();
+
+const stringRoles = computed(() => Object.values(MemberRole).filter((value) => typeof value === 'string'));
 </script>
 
 <style scoped>
