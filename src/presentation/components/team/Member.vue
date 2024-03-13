3e38de94dd797c5a325992d121d836d54347aae8
elizachi <lizachig2003@gmail.com>
2024-03-13 20:13:21 +0300

    Now we can change member role in real time

--- src/presentation/components/team/Member.vue
+++ src/presentation/components/team/Member.vue
@@ -3,32 +3,25 @@
     <div class="member-name">
       {{ teamMember.user.name || teamMember.user.email }}
     </div>
-    <select>
+    <select
+      v-model="selectedRole"
+      @change="updateMemberRole($event)"
+    >
       <option
-        v-for="(role, index) in stringRoles"
+        v-for="(role, index) in roleOptions"
         :key="index"
-        :selected="teamMember.role == index"
       >
         {{ role }}
       </option>
     </select>
-    <!-- <div v-if="teamMember.id != 1">
-      <Checkbox
-        :checked="teamMember.role === 1"
-        :label="t('noteSettings.team.canEdit')"
-        @update:checked="changeMemberRole"
-      />
-    </div> -->
   </li>
 </template>
 
 <script setup lang="ts">
-import Checkbox from '@/presentation/components/checkbox/Checkbox.vue';
 import { MemberRole, TeamMember } from '@/domain/entities/Team.ts';
-import { useI18n } from 'vue-i18n';
-import useNoteSettings from '@/application/services/useNoteSettings';
 import { NoteId } from '@/domain/entities/Note';
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
+import useNoteSettings from '@/application/services/useNoteSettings';
 
 /**
  * TeamMember props
@@ -44,18 +37,21 @@ const props = defineProps<{
   id: NoteId;
 }>();
 
+const selectedRole = ref(MemberRole[props.teamMember.role]);
+
+const roleOptions = computed(() => Object.values(MemberRole).filter((value) => typeof value === 'string'));
+
 const { changeRole } = useNoteSettings();
 
 /**
- * Change member role
+ * Updates the user role if it has been changed
+ *
+ * @param event
  */
-async function changeMemberRole() {
-  changeRole(props.id, props.teamMember.user.id, props.teamMember.role);
+async function updateMemberRole(event: Event) {
+  changeRole(props.id, props.teamMember.user.id, MemberRole[(event.target as HTMLSelectElement).value as string]);
+  console.log((event.target as HTMLSelectElement).value);
 }
-
-const { t } = useI18n();
-
-const stringRoles = computed(() => Object.values(MemberRole).filter((value) => typeof value === 'string'));
 </script>
 
 <style scoped>
