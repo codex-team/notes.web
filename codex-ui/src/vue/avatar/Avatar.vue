987d6503dfe97b2c8ce7c6d3ede5cebc3c2265cc
Ivan Skvorcov <mainpostskvorcov@gmail.com>
2024-03-26 15:10:51 +0300

    Add base avatar component

---
+++ codex-ui/src/vue/avatar/Avatar.vue
@@ -0,0 +1,22 @@
+<template>
+    <img :src="props.src" alt="avatar" :class="$style.avatar">
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps<{
+  /**
+   * Path to the image
+   */
+  src: string;
+}>();
+</script>
+
+<style module>
+.avatar {
+  width: var(--size-avatar);
+  height: var(--size-avatar);
+  border-radius: var(--radius-m);
+}
+</style>
--- codex-ui/src/vue/index.ts
+++ codex-ui/src/vue/index.ts
@@ -2,5 +2,6 @@ import Button from './button/Button.vue';
 import Input from './input/Input.vue';
 import Heading from './heading/Heading.vue';
 import FormField from './form/Field.vue';
+import Avatar from './avatar/Avatar.vue';
 
-export { Button, Input, Heading, FormField };
+export { Button, Input, Heading, FormField, Avatar };
