e537b9f639ba17c7bb95bf6645749e9c0fbf40dd
e11sy <Egoramurin@gmail.com>
2024-04-13 19:38:13 +0300

    get rid of default class for switch

--- codex-ui/src/vue/switch/Switch.types.ts
+++
@@ -1,9 +0,0 @@
-/**
- * States of the switch
- */
-export type Disabled = true | false;
-
-/**
- * Values of the switch
- */
-export type SwitchOn = true | false;
--- codex-ui/src/vue/switch/Switch.vue
+++ codex-ui/src/vue/switch/Switch.vue
@@ -1,38 +1,37 @@
 <template>
   <div
-    :class="[$style.switch, $style[`switch-${switchValue}-${disabled ? 'disabled' : 'default'}`]]"
+    :class="[
+      $style.switch,
+      $style[`switch--${switchValue}`],
+      disabled ? $style[`switch--${switchValue}--disabled`] : '',
+    ]"
     v-on="disabled === false ? { click: changeValue } : {}"
   >
     <div
-      :class="[
-        $style[`switch__ellipse`],
-        $style[`switch__ellipse-${disabled ? 'disabled' : 'default'}`],
-        $style[`switch-${switchValue}`],
-      ]"
+      :class="[$style[`switch__ellipse${disabled ? '--disabled' : ''}`], $style[`switch__ellipse--${switchValue}`]]"
     ></div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { SwitchOn, Disabled } from './Switch.types.js';
-import { ref } from 'vue';
-
 const props = withDefaults(
   defineProps<{
     /**
      * Value of the switch ('on' or 'off')
      */
-    value?: SwitchOn;
+    value?: boolean;
 
     /**
      * State of interaction with the component
      */
-    disabled?: Disabled;
+    disabled?: boolean;
   }>(),
   { value: true, disabled: false }
 );
 
-const switchValue = ref(props.value);
+const switchValue = defineModel({ default: true });
+
+switchValue.value = props.value;
 
 /**
  * Function for changing value of the switch on click
@@ -55,47 +54,51 @@ function changeValue() {
   &__ellipse {
     width: 18px;
     height: 18px;
+    transition: background 2s;
     border-radius: var(--radius-l);
-    &-disabled {
-      background: var(--accent--text-secondary);
+    background: var(--accent--text-solid-foreground);
+
+    &--true {
+      margin-left: calc(100% - 18px);
+      transition: margin-left 0.2s;
     }
-    &-default {
-      background: var(--accent--text-solid-foreground);
+
+    &--false {
+      margin-left: 0;
+      transition: margin-left 0.2s;
+    }
+
+    &--disabled {
+      width: 18px;
+      height: 18px;
+      border-radius: var(--radius-l);
+      background: var(--accent--text-secondary);
     }
   }
 
   /* Style for true value of the switch*/
-  &-true {
-    margin-left: calc(100% - 18px);
-    transition: margin-left 0.2s;
+  &--true {
+    --bg-color: var(--accent--solid);
 
-    &-default {
-      --bg-color: var(--accent--solid);
-      &:hover {
-        background-color: var(--base--solid-hover);
-      }
+    &:hover {
+      --bg-color: var(--base--solid-hover);
     }
 
-    &-disabled {
+    &--disabled {
       --bg-color: var(--accent--bg-secondary);
       cursor: not-allowed;
     }
   }
 
   /* Style for false value of the switch*/
-  &-false {
-    margin-left: 0;
-    transition: margin-left 0.2s;
+  &--false {
+    --bg-color: var(--accent--bg-secondary);
 
-    &-default {
-      --bg-color: var(--accent--bg-secondary);
-
-      &:hover {
-        background-color: var(--accent--bg-secondary-hover);
-      }
+    &:hover {
+      --bg-color: var(--accent--bg-secondary-hover);
     }
 
-    &-disabled {
+    &--disabled {
       --bg-color: var(--accent--bg-secondary);
       cursor: not-allowed;
     }
