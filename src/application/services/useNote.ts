20c7fbab5b373f16e9ae5808ef72a212e9c8f158
e11sy <Egoramurin@gmail.com>
2024-05-11 16:07:10 +0300

    update dependencies and run linter

--- src/application/services/useNote.ts
+++ src/application/services/useNote.ts
@@ -179,7 +179,7 @@ export default function (options: UseNoteComposableOptions): UseNoteComposableSt
     }
 
     const usedNoteTools = content.blocks.map((block) => {
-      const blockTool = (tools.value as EditorTool[]).find((tool) => tool.name === block.type)
+      const blockTool = (tools.value as EditorTool[]).find((tool) => tool.name === block.type);
 
       /**
        * User can not add to content tool that is not in allTools
