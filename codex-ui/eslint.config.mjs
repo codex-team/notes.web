dae190b5dd31236d0bc1894fcab772190ca67d3c
e11sy <Egoramurin@gmail.com>
2024-06-05 19:06:13 +0300

    added eslint config for codex-ui derectory

---
+++ codex-ui/eslint.config.mjs
@@ -0,0 +1,78 @@
+import CodeX from 'eslint-config-codex';
+import { plugin as TsPlugin, parser as TsParser } from 'typescript-eslint';
+import VueParser from 'vue-eslint-parser';
+
+/**
+ * @todo connect architecture config
+ */
+export default [
+  ...CodeX,
+  {
+    name: 'ts-codex-ui',
+    ignores: ['*.pcss', '*.otf', 'eslint.config.mjs', 'postcss.config.js'],
+    plugins: {
+      '@typescript-eslint': TsPlugin,
+    },
+
+    languageOptions: {
+      parser: TsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: './',
+        sourceType: 'module', // Allows for the use of imports
+      },
+    },
+
+    rules: {
+      'n/no-missing-import': ['off'],
+      'n/no-unpublished-import': ['error', {
+        allowModules: ['vitest', 'postgres-migrations', 'eslint-import-resolver-alias', 'eslint-config-codex'],
+        ignoreTypeImport: true,
+      }],
+      'n/no-unsupported-features/node-builtins': ['error', {
+        version: '>=22.1.0',
+      }],
+      'n/no-extraneous-import': ['error', {
+        allowModules: ['@editorjs/editorjs', 'typescript-eslint', 'vue-eslint-parser'],
+      }],
+
+      /**
+       * @todo get rid of this rule ignores and solve all eslint errors occured
+       */
+      '@typescript-eslint/no-unsafe-assignment': ['off'],
+      '@typescript-eslint/no-unsafe-argument': ['off'],
+      '@typescript-eslint/no-unsafe-return': ['off'],
+      '@typescript-eslint/no-unsafe-call': ['off'],
+      '@typescript-eslint/no-unsafe-member-access': ['off'],
+      'jsdoc/require-param-type': ['off'],
+      'jsdoc/informative-docs': ['off'],
+      'jsdoc/require-jsdoc': ['off'],
+    },
+  },
+  {
+    languageOptions: {
+      parser: VueParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        extraFileExtensions: ['.vue'],
+        parser: TsParser,
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'n/no-missing-import': ['off'],
+      'n/no-unpublished-import': ['error', {
+        allowModules: ['vitest', 'postgres-migrations', 'eslint-import-resolver-alias', 'eslint-config-codex'],
+        ignoreTypeImport: true,
+      }],
+      'n/no-unsupported-features/node-builtins': ['error', {
+        version: '>=22.1.0',
+      }],
+      'jsdoc/require-param-type': ['off'],
+      'jsdoc/informative-docs': ['off'],
+      'jsdoc/require-jsdoc': ['off'],
+    },
+  },
+];
