f55e0bdb70359857915c4258d5e78c65bc087361
e11sy <Egoramurin@gmail.com>
2024-06-29 21:26:40 +0300

    improved typografy

--- codex-ui/dev/Playground.vue
+++ codex-ui/dev/Playground.vue
@@ -235,31 +235,31 @@
       Tab
     </Heading>
     <Tab
-      :title="'Home'"
-      :url="'/'"
-    />
-    <br>
-    <Tab
-      :url="'/'"
-      :title="'Home'"
-      :picture="'../static/example-avatar.png'"
-    />
-    <br>
-    <Tab
-      :url="'/'"
       title="Home"
-      :icon="'Plus'"
+      url="/"
     />
     <br>
     <Tab
-      :url="'/'"
-      :title="'Home'"
+      url="/"
+      title="Home"
+      picture="../static/example-avatar.png"
+    />
+    <br>
+    <Tab
+      url="/"
+      title="Home"
+      icon="Plus"
+    />
+    <br>
+    <Tab
+      url="/"
+      title="Home"
       :closable="true"
     />
     <br>
     <Tab
-      :url="'/'"
-      :title="'Home'"
+      url="/"
+      title="Home"
       :closable="true"
       :is-active="true"
     />
