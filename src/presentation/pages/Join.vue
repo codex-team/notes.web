c90df9990a0f60ffcf626952435d8f1ca36a9ebb
e11sy <Egoramurin@gmail.com>
2024-07-04 16:30:39 +0300

    added join route handler

--- src/presentation/pages/Join.vue
+++ src/presentation/pages/Join.vue
@@ -1,36 +1,70 @@
 <template>
   <Heading :level="1">
-    You are joining
+    {{ message }}
   </Heading>
 </template>
 
 <script setup lang="ts">
 import { Heading } from 'codex-ui/vue';
 import { useRouter } from 'vue-router';
-import { onMounted, ref } from 'vue';
+import { ref, watch } from 'vue';
 import useTeam from '@/application/services/useTeam';
+import { useAppState } from '@/application/services/useAppState';
+import useAuth from '@/application/services/useAuth';
 import type { TeamMember } from '@/domain/entities/Team';
 import { InvitationHash } from '@/domain/entities/NoteSettings';
+import { useI18n } from 'vue-i18n';
+
+const { user } = useAppState();
+const { t } = useI18n();
+const { showGoogleAuthPopup } = useAuth();
+const { joinNoteTeamByHash } = useTeam();
+const router = useRouter();
 
 const props = defineProps<{
   invitationHash: InvitationHash;
 }>();
 
-const { joinNoteTeamByHash } = useTeam();
+/**
+ * Message to be displayed as a heading of join page
+ */
+const message = ref(t('join.title'));
 
-const router = useRouter();
+const teamMember = ref<TeamMember | null>(null);
 
-onMounted(async () => {
-  const teamMember = ref<TeamMember | null | undefined>(undefined);
+async function handleJoin(): Promise<void> {
+  /**
+   * Check if user is authorized
+   */
+  if (user.value !== null) {
+    teamMember.value = await joinNoteTeamByHash(props.invitationHash);
+    /**
+     * Check if we got id of the note to redirect
+     */
+    if (teamMember.value?.noteId) {
+      router.push(`/note/${teamMember.value?.noteId}`);
+    } else {
+      /**
+       * Join made teamMember null
+       * Means that join link is expired
+       */
+      message.value = t('join.linkExpired');
+    }
+  } else {
+    /**
+     * Authorize if not authorized yet
+     */
+    showGoogleAuthPopup();
+  }
+};
 
-  teamMember.value = await joinNoteTeamByHash(props.invitationHash);
+/**
+ * Watching authorization of the user
+ */
+watch(user, async () => {
+  await handleJoin();
+}, { immediate: true });
 
-  console.log('member', teamMember.value);
-
-  if (teamMember.value?.noteId) {
-    router.push(`/note/${teamMember.value?.noteId}`);
-  };
-});
 </script>
 
 <style></style>
