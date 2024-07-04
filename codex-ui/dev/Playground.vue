0f6eb6cc0e86bedcb83c9f309523a6d947055ecd
iamgabrielsoft@gmail.com <iamgabrielsoft@gmail.com>
2024-07-04 20:01:35 +0100

    lint file working on useToast hook

--- codex-ui/dev/Playground.vue
+++ codex-ui/dev/Playground.vue
@@ -199,10 +199,10 @@ const pages = computed(() => [
       },
 
       {
-        title: 'Alert', 
+        title: 'Alert',
         onActivate: () => router.push('/components/alert'),
-        isActive: route.path === '/components/alert'
-      }
+        isActive: route.path === '/components/alert',
+      },
     ],
   },
 ]);
@@ -238,7 +238,6 @@ const pages = computed(() => [
   text-decoration: none;
   color: var(--base--text);
 }
-
 
 .body {
   padding: var(--spacing-l);
