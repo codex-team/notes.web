7db598886ef964ec73904a44e672234d3ee5fc7e
elizachi <chigvintseva.elizaveta@gmail.com>
2024-07-05 05:02:16 +0300

    Added close button

--- codex-ui/src/vue/components/popup/Popup.vue
+++ codex-ui/src/vue/components/popup/Popup.vue
@@ -3,10 +3,15 @@
     <div :class="$style['container']">
       <slot />
     </div>
+    <div :class="$style['popup__icon']">
+      <Icon name="Cross" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import Icon from '../icon/Icon.vue';
+
 withDefaults(
   defineProps<{
     /**
@@ -25,11 +30,23 @@ withDefaults(
   position: fixed;
   top: 0;
   left: 0;
-  bottom: 0;
   right: 0;
+  bottom: 0;
+  z-index: 100;
+  display: flex;
   gap: var(--spacing-ms);
   background-color: rgba(0, 0, 0, 0.49);
-  z-index: 100;
+
+  &__icon {
+  position: absolute;
+  top: 50%;
+  color: var(--base--text-secondary);
+  right: calc(50% + var(--spacing-very-x));
+  transform: translateY(-50%);
+  padding: var(--spacing-very-x);
+  border: var(--delimiter-height) solid;
+  border-radius: var(--radius-l);
+}
 }
 
 .container {
@@ -38,9 +55,8 @@ withDefaults(
   position: absolute;
   gap: var(--spacing-ml);
   padding: var(--spacing-l);
+  border: var(--delimiter-height) solid var(--base--border);
   border-radius: var(--radius-ml);
-  border-color: var(--base--border);
-  border-width: var(--delimiter-height);
   background-color: var(--base--bg-primary);
 }
 </style>
