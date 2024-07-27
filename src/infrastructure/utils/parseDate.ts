63305c5259789e67a4c137344734799614cda512
e11sy <Egoramurin@gmail.com>
2024-07-27 17:24:19 +0300

    imroved parseDate util

--- src/infrastructure/utils/parseDate.ts
+++ src/infrastructure/utils/parseDate.ts
@@ -1,19 +1,8 @@
 export function parseDate(date: Date): string {
-  // Массив с названиями месяцев
-  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-  // Получаем название месяца
-  const month = months[date.getUTCMonth()];
-
-  // Получаем день
-  const day = date.getUTCDate();
-
-  // Получаем часы и минуты, добавляем ведущий ноль при необходимости
-  const hours = date.getUTCHours().toString()
-    .padStart(2, '0');
-  const minutes = date.getUTCMinutes().toString()
-    .padStart(2, '0');
-
-  // Формируем итоговую строку
-  return `${month} ${day}, ${hours}:${minutes}`;
+  return new Intl.DateTimeFormat('en-GB', {
+    month: 'short',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+  }).format(date);
 }
