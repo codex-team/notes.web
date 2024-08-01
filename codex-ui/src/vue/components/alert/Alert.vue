8ebbcd56af0396a9c2684a0a5741908de9fd3af5
iamgabrielsoft@gmail.com <iamgabrielsoft@gmail.com>
2024-08-01 18:16:08 +0100

    lint file

--- codex-ui/src/vue/components/alert/Alert.vue
+++ codex-ui/src/vue/components/alert/Alert.vue
@@ -1,17 +1,16 @@
 <template>
-  
-  <transition name="fadeInBottom" >
-    <div class="alert" :class="`alert--${alertType}`" >
-      <!-- imported icon testing purpose -->
-      <Icon name="Collapse"/>
-      <span v-if="props.message">{{ message  }}</span>
+  <transition name="fadeInBottom">
+    <div
+      v-show="message"
+      class="alert"
+      :class="`alert--${alertType}`"
+    >
+      <Icon name="Check" />
+      <span v-if="props.message">{{ message }}</span>
       <slot />
     </div>
   </transition>
-
 </template>
-
-
 
 <script setup lang="ts">
 import { defineProps, computed, withDefaults } from 'vue';
@@ -22,47 +21,39 @@ const props = withDefaults(
   /**
    * message to display
    */
-  message: string, 
+    message?: string;
 
-  /**
-   * alert status 
-   */
-  type: 'success' | 'error' | 'warning' | 'default' | 'info'
+    /**
+     * alert status
+     */
+    type: 'success' | 'error' | 'warning' | 'default' | 'info';
 
-  /**
-   * where to position the alert on the screen
-   */
-  position: 'bottom-centera'
-}>(), {
-  message: '', 
-  type: 'default', 
-  position: 'bottom-center'
-}
-)
-
+  }>(), {
+    message: '',
+    type: 'default',
+  }
+);
 
 /** determine alert status */
 const alertType = computed(() => {
-  if(props.type === 'success'){
-    console.log('succ')
-    return 'success'; 
+  if (props.type === 'success') {
+    return 'success';
   }
 
-  if(props.type === 'error'){
-    return 'error'; 
+  if (props.type === 'error') {
+    return 'error';
   }
 
-  if(props.type === 'warning'){
+  if (props.type === 'warning') {
     return 'warning';
   }
 
-  if(props.type === 'info'){
+  if (props.type === 'info') {
     return 'info';
   }
 
   return 'default';
 });
-
 
 </script>
 
@@ -80,16 +71,14 @@ const alertType = computed(() => {
   }
 }
 
-
 .alert {
   --padding-left: var(--h-padding);
   --padding-right: var(--h-padding);
   --success: #00A64D;
   --error: #BB393D;
   --warning: #DE6205;
-  --info: #54617B; 
+  --info: #54617B;
   --default: #54617B;
-
 
   position: fixed;
   bottom: 0;
@@ -101,14 +90,12 @@ const alertType = computed(() => {
   gap: var(--v-padding);
   border: 0;
   outline: 0;
-  font-family: inherit; 
+  font-family: inherit;
   cursor: text;
   word-break: keep-all;
   z-index: 1;
   border-radius: var(--radius-field);
   padding: 1rem;
-
-
 
   &--success {
     background-color: var(--success);
