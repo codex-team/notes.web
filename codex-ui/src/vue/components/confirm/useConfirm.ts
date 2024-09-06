fb7215fa20f4695984ef389589721f1e03e06f30
elizachi <chigvintseva.elizaveta@gmail.com>
2024-09-06 23:08:12 +0300

    get rid of result ref

--- codex-ui/src/vue/components/confirm/useConfirm.ts
+++ codex-ui/src/vue/components/confirm/useConfirm.ts
@@ -1,15 +1,8 @@
 import { createSharedComposable } from '@vueuse/core';
 import { usePopup } from '../popup';
-import { ref } from 'vue';
 import { Confirm } from '.';
 
 export const useConfirm = createSharedComposable(() => {
-  /**
-   * Represents the result of the user's selection: false - Cancel button was pressed,
-   * true - Confirm button was pressed, undefined - no selection has been made yet
-   */
-  const result = ref<boolean>();
-
   /**
    * Used to create a Popup component that will display the current Confirm
    */
@@ -21,30 +14,23 @@ export const useConfirm = createSharedComposable(() => {
    * @returns user selection result
    */
   async function confirm(title: string, text: string): Promise<boolean> {
-    showPopup({
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      component: Confirm,
-      props: {
-        title: title,
-        text: text,
-        onCancel: () => {
-          result.value = false;
-        },
-        onConfirm: () => {
-          result.value = true;
-        },
-      },
-    });
-
-    /**
-     * Check if the user has pressed the confirm or close button
-     */
     return new Promise<boolean>((resolve) => {
-      if (result.value !== undefined) {
-        resolve(result.value);
-      }
+      showPopup({
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        component: Confirm,
+        props: {
+          title: title,
+          text: text,
+          onCancel: () => {
+            resolve(false);
+          },
+          onConfirm: () => {
+            resolve(true);
+          },
+        },
+      });
     });
-  };
+  }
 
   return {
     confirm,
