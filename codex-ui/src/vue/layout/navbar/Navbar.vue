03ba2ceb741f979945f963baf4c511a45e9e6366
Dobrunia Kostrigin <48620984+Dobrunia@users.noreply.github.com>
2024-12-24 17:22:52 +0300

    fixed 1 space indentation

--- codex-ui/src/vue/layout/navbar/Navbar.vue
+++ codex-ui/src/vue/layout/navbar/Navbar.vue
@@ -9,20 +9,20 @@
 
 <style scoped lang="postcss">
 .navbar {
-    position: sticky;
-    top: 0;
-    z-index: var(--z-navbar);
-    background-color: var(--base--bg-primary);
-    display: flex;
-    align-items: center;
-    width: 100%;
-    max-width: 100%;
-    height: var(--layout-navbar-height);
-    border-bottom: 1px solid var(--base--border);
-    box-sizing: border-box;
+  position: sticky;
+  top: 0;
+  z-index: var(--z-navbar);
+  background-color: var(--base--bg-primary);
+  display: flex;
+  align-items: center;
+  width: 100%;
+  max-width: 100%;
+  height: var(--layout-navbar-height);
+  border-bottom: 1px solid var(--base--border);
+  box-sizing: border-box;
 
-    &__right {
-        margin-left: auto;
-    }
+  &__right {
+      margin-left: auto;
+  }
 }
 </style>
